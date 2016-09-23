79ed0746f46406d0eb6773d2b43e93dae24b67ad
Stanley Lio <stanleylio@gmail.com>
2016-09-23 05:39:59 +0000

    .

---
+++ meta.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('/root/node')
+from datetime import datetime
+import logging,traceback
+from display.gen_plot import plot_time_series
+from os.path import join,basename
+
+
+logging.basicConfig()
+
+
+with open('/var/logging/log/spaceusage.log') as f:
+    D = {}
+    ts = None
+    for line in f:
+        line = line.strip()
+        try:
+            ts = datetime.strptime(line,'%Y-%m-%dT%H:%M:%SZ')
+            continue
+        except ValueError:
+            logging.debug(traceback.format_exc())
+
+        assert ts is not None
+        line = line.split('\t')
+        if line[1] not in D:
+            D[line[1]] = []
+        D[line[1]].append([ts,float(line[0])])
+
+
+for k in D.keys():
+    d = zip(*D[k])
+    plot_time_series(d[0],[v/1e6 for v in d[1]],\
+                     join('/var/www/km1app/km1app/static/img','space_' + basename(k) + '.png'),\
+                     title=k,xlabel='Logger Time (UTC)',ylabel='Directory Size, MB',\
+                     markersize=8)
+
--- r2t.py
+++ r2t.py
@@ -1,12 +1,14 @@
 import math
 
 def r2t(R):
+    """PIR conversion from resistance to temperature"""
     C1 = 0.0010295
     C2 = 0.0002391
     C3 = 0.0000001568
     return 1/(C1 + C2*math.log(R) + C3*(math.log(R)**3)) - 273.15
 
 def v2r(V,Vref=2.5):
+    """PIR conversion from voltage to resistance"""
     if Vref <= V:
         return float('nan')
     return 10e3*V/(Vref-V)
