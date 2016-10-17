53f4ce2ac3549c73e5b95ecbf8b813bfaf82ccc6
Stanley Lio <stanleylio@gmail.com>
2016-10-17 08:14:44 +0000

    .

--- service_discovery.py
+++ service_discovery.py
@@ -14,9 +14,11 @@
 # SOEST, University of Hawaii
 # All Rights Reserved, 2016
 import sys,time,json,subprocess,traceback,socket,logging
+from datetime import datetime
+from twisted.internet.protocol import DatagramProtocol
 
 
-def taskServiceBroadcast():
+'''def taskServiceBroadcast():
     try:
         # get own IP
         # assuming there's only one interface...
@@ -34,21 +36,75 @@ def taskServiceBroadcast():
         s.sendto(line,('<broadcast>',9005))
 
         logging.debug(line)
-
-        time.sleep(60)
     except KeyboardInterrupt:
         raise
     except:
-        logging.debug(traceback.format_exc())
+        logging.debug(traceback.format_exc())'''
+
+
+publishers = {}
+
+
+class Noname(DatagramProtocol):
+    def startProtocol(self):
+        self.transport.setBroadcastAllowed(True)
+    
+    def datagramReceived(self,data,(host,port)):
+        #logging.debug((data,host,port))
+        #self.transport.write(data,(host,port))
+        try:
+            d = json.loads(data)
+            if 'services' in d:
+                for s in d['services']:
+                    if 'kmet1' == s[0]:
+                        logging.debug('Found publisher: ' + str(s))
+                        ts = datetime.utcnow()
+                        publishers[d['hostname']] = (ts,s[1],s[2])
+                #print publishers
+
+            if 'kmet1' == d.get('service_query',None):
+                logging.debug('Responding to query...')
+                proc = subprocess.Popen(['hostname -I'],stdout=subprocess.PIPE,shell=True)
+                out,err = proc.communicate()
+                ips = out.strip().split(' ')
+                ips = filter(lambda x: x != '192.168.7.2',ips)[0]
+
+                d = {'hostname':socket.gethostname(),'services':[['kmet1',ips,9002]]}
+                line = json.dumps(d,separators=(',',':'))
+
+                self.transport.write(line,(host,port))
+        except:
+            logging.debug(traceback.format_exc())
+
+    def service_query(self,topic='kmet1'):
+        logging.debug('Broadcasting query...')
+        line = json.dumps({'service_query':topic},separators=(',',':'))
+        self.transport.write(line,('<broadcast>',9005))
+            
+
+def taskServiceQuery():
+    p.service_query()
 
 
 if '__main__' == __name__:
     from twisted.internet.task import LoopingCall
     from twisted.internet import reactor
+    import sys
+
+    if len(sys.argv) > 1:
+        period = float(sys.argv[1])
+    else:
+        period = 60
 
     logging.basicConfig(level=logging.DEBUG)
 
-    lcsb = LoopingCall(taskServiceBroadcast)
-    lcsb.start(60)
+    #lcsb = LoopingCall(taskServiceBroadcast)
+    #lcsb.start(period)
 
+    p = Noname()
+
+    lcsq = LoopingCall(taskServiceQuery)
+    lcsq.start(1,now=False)
+
+    reactor.listenUDP(9005,p)
     reactor.run()
