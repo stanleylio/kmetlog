a021e6312deb078522803792201b840de340545b
Stanley Lio <stanleylio@gmail.com>
2016-10-20 20:23:54 +0000

    .

--- service_discovery.py
+++ service_discovery.py
@@ -1,54 +1,31 @@
 # Service discovery stuff
 #
-# To find other publishers, broadcast UDP at 9005 the json string (topic = 'kmet1' as example)
+# To find other publishers, broadcast UDP at 9005 the json string (using topic='kmet1' as example)
 #   {'service_query':'kmet1'}
 # Publishers that got this message would respond the json string (kmet-bbb as example)
 #   {'hostname':'kmet-bbb','services':[['kmet1','192.168.1.109',9002]]}
 #
 # Wouldn't need this if DHCP works.
 #
-# ... there's no limit to the complexity - a host can hosts multiple services at multiple ports...
-# or the full-blown N-to-N, "every host has potentially sensor feeds that other hosts can read", and
+# There's no limit to complexity - a publisher can publish multiple topics at multiple ports, and
+# a subscriber can be subscribed to multiple topics from multiple publishers.
+# Basically full-blown N-to-N, "every host has potentially sensor feeds that other hosts can read", and
 # "every host is potentially interested in every other feed" - i.e., there's no longer fixed roles
-# of "publisher" and "logger"
+# of "publisher" and "data logger".
 #
 # There's still the decision to be made: after discovery, which of those am I actually interested in?
 #
-# Stanley H.I Lio
+# Stanley H.I. Lio
 # hlio@hawaii.edu
 # Ocean Technology Group
 # SOEST, University of Hawaii
 # All Rights Reserved, 2016
 import sys,json,subprocess,traceback,socket,logging
 from twisted.internet.protocol import DatagramProtocol
-from datetime import datetime
+from datetime import datetime,timedelta
 
 
-'''def taskServiceBroadcast():
-    try:
-        # get own IP
-        # assuming there's only one interface...
-        proc = subprocess.Popen(['hostname -I'],stdout=subprocess.PIPE,shell=True)
-        out,err = proc.communicate()
-        ips = out.strip().split(' ')
-        ips = filter(lambda x: x != '192.168.7.2',ips)[0]
-
-        d = {'hostname':socket.gethostname(),'services':[['kmet1',ips,9002]]}
-        line = json.dumps(d,separators=(',',':'))
-
-        s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        s.bind(('',0))
-        s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
-        s.sendto(line,('<broadcast>',9005))
-
-        logging.debug(line)
-    except KeyboardInterrupt:
-        raise
-    except:
-        logging.debug(traceback.format_exc())'''
-
-
-publishers = {}     # the current list of known publishers
+topic = 'kmet1'
 
 
 def getIP():
@@ -61,43 +38,68 @@ def getIP():
 
 class ServiceDiscovery(DatagramProtocol):
     _hostname = socket.gethostname()
+    _publishers = {}     # the current list of known publishers
     
     def startProtocol(self):
         self.transport.setBroadcastAllowed(True)
     
     def datagramReceived(self,data,(host,port)):
+        """Process incoming messages. Expecting two types of messages:
+1. it's a "service_query": respond if it's looking for the topic 'kmet1';
+2. it's a "service_response": add this publisher if it has the topic 'kmet1'"""
         #logging.debug((data,host,port))
         #self.transport.write(data,(host,port))
         try:
             d = json.loads(data)
 
+            #if d['hostname'] == self._hostname:   # exclude self
+                #logging.debug('ignore msg from oneself: ' + data)
+                #return
+
             if 'service_response' in d:
-                #if d['hostname'] != self._hostname:   # exclude self
+                if 'hostname' not in d:
+                    logging.debug('"hostname" missing: ' + data)
+                    return
+
                 # basically it's not up to the driver to decide whether to list oneself as a publisher
                 for s in d['service_response']:
-                    if 'kmet1' == s[0]:
-                        logging.debug('Found publisher: ' + str(s))
-                        dt = datetime.utcnow()
-                        if s[1] == host:
-                            # no spoofing? now only the publisher can announce its own presence. that means
-                            # you can't have a device that only provide "service listing"...
-                            publishers[d['hostname']] = (dt,s[1],s[2])
-                #print publishers
-                #else:
-                    #logging.debug('ignore self')
-                    #pass
-            elif 'service_query' in d:
-                if 'kmet1' == d['service_query']:
+                    if topic != s[0]:
+                        logging.debug("not what I'm interested in: " + data)
+                        return
+                    
+                    # No spoofing? Now only the publisher itself can announce its own presence.
+                    # That means you can't have dedicated "service listing provider" (which
+                    # doesn't itself publish).
+                    #
+                    # If only publisher itself can announce its own presence, then the response
+                    # doesn't have to include ip or port since you know where it comes from, and
+                    # the port is fixed across the entire project.
+                    if s[1] != host:
+                        logging.debug("announcement not coming from the publisher itself: " + data)
+                        return
+
+                    #logging.info('Found publisher: ' + str(s))
+                    if d['hostname'] not in self._publishers:
+                        logging.info('Added publisher: ' + d['hostname'] + ', ' + str(s))
+                    
+                    self._publishers[d['hostname']] = (datetime.utcnow(),s[1],s[2])
+
+                    #print self._publishers
+            
+            if 'service_query' in d:
+                if topic == d['service_query']:
                     logging.debug('Responding to query...')
                     ip = getIP()
 
-                    #if ip != host:  # the query originated from oneself
-                    d = {'hostname':self._hostname,'service_response':[['kmet1',ip,9002]]}
+                    d = {'hostname':self._hostname,'service_response':[[topic,ip,9002]]}
+                    # in an alternate universe...
+                    #d = {'hostname':self._hostname,'service_response':[topic1,topic2,topic3...]}
                     line = json.dumps(d,separators=(',',':'))
 
                     self.transport.write(line,(host,port))
         except:
             logging.debug(traceback.format_exc())
+            logging.debug(data)
 
     def service_query(self,topic='kmet1'):
         """Query everyone for the given topic via UDP broadcast.
@@ -105,7 +107,16 @@ Whoever publishing this topic would respond with its own IP."""
         logging.debug('Broadcasting query...')
         line = json.dumps({'service_query':topic},separators=(',',':'))
         self.transport.write(line,('<broadcast>',9005))
-            
+
+    def get_publisher_list(self):
+        # remove stale entries
+        for k in self._publishers.keys():
+            host = self._publishers[k]
+            if datetime.utcnow() - host[0] > timedelta(minutes=30):
+                 del self._publishers[k]
+        #print self._publishers
+        return self._publishers
+
 
 if '__main__' == __name__:
     from twisted.internet.task import LoopingCall
@@ -115,18 +126,16 @@ if '__main__' == __name__:
     if len(sys.argv) > 1:
         period = float(sys.argv[1])
     else:
-        print('psst: you can set the period to N seconds by "python service_discovery.py N"')
+        print('(psst: you can set the period to N seconds by "python service_discovery.py N")')
         period = 1
 
     logging.basicConfig(level=logging.DEBUG)
-
-    #lcsb = LoopingCall(taskServiceBroadcast)
-    #lcsb.start(period)
+    #logging.basicConfig(level=logging.INFO)
 
     p = ServiceDiscovery()
 
-    lcsq = LoopingCall(p.service_query)
-    lcsq.start(period,now=False)
+    LoopingCall(p.service_query).start(period,now=False)
+    #LoopingCall(p.get_publisher_list).start(2,now=True)
 
     reactor.listenUDP(9005,p)
     reactor.run()
