518587443ea75063cdacaea3f4ad544a52c8d15c
Stanley Lio <stanleylio@gmail.com>
2016-11-04 10:44:48 +0000

    .

--- ping.py
+++ ping.py
@@ -1,8 +1,11 @@
-import sys,socket
+import sys,socket,time
 from os.path import expanduser
 sys.path.append(expanduser('~'))
 from node.send2server import post
 from os.path import expanduser
+import dbstuff
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 m = socket.gethostname()
@@ -10,9 +13,6 @@ print(post(m,'http://grogdata.soest.hawaii.edu/api/4'))
 exit()
 
 
-import dbstuff
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 
 dbname = 'kmetlog'
@@ -28,6 +28,7 @@ Session.configure(bind=engine)
 session = Session()
 
 #tags = [table.name for table in meta.sorted_tables]
-res = session.query('BME280').all()
-print res[1].title
+#res = session.query('BME280').all()
+print session.query(dbstuff.BME280_Sample).\
+      filter(dbstuff.BME280_Sample.ts >= time.time()-3600).all()
 
