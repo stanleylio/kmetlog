881ab827132c0b0ef65000de59ae426351778b12
Stanley Lio <stanleylio@gmail.com>
2017-02-18 22:37:41 +0000

    clean up

--- zmq2ws.py
+++ zmq2ws.py
@@ -1,98 +1,57 @@
-import zmq,logging,time
-import logging.handlers
-from os import makedirs
-from os.path import exists,join
+# redirect stuff from zmq to ws
+#
+# Stanley H.I. Lio
+# hlio@hawaii.edu
+# All Rights Reserved. 2017
+import zmq,sys
 from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
+sys.path.append('..')
+from node.wsrelay import BroadcastServerProtocol,BroadcastServerFactory
 from autobahn.twisted.websocket import WebSocketServerFactory,\
      WebSocketServerProtocol,\
      listenWS
-import config
 
 
-log_path = '/var/kmetlog/log'
-if not exists(log_path):
-    makedirs(log_path)
+if 3 != len(sys.argv):
+    print('usage: python zmq2ws.py zmq_port ws_port')
+    print('example: python zmq2ws.py 9002 9000')
+    exit()
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-fh = logging.handlers.RotatingFileHandler(join(log_path,'zmq2ws.log'),
-                                          maxBytes=1e7,
-                                          backupCount=3)
-fh.setLevel(logging.WARNING)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-logging.Formatter.converter = time.gmtime
-formatter = logging.Formatter('%(asctime)s,%(name)s,%(levelname)s,%(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-logger.addHandler(fh)
-logger.addHandler(ch)
+try:
+    a,b = int(sys.argv[1]),int(sys.argv[2])
+    zmq_port,ws_port = a,b
+except:
+    print('bad arguments: {}'.format(sys.argv[1:]))
+    exit()
 
+print('zmq: {} -> ws: {}'.format(zmq_port,ws_port))
 
-class BroadcastServerProtocol(WebSocketServerProtocol):
-    def onOpen(self):
-        self.factory.register(self)
-
-    def onMessage(self, payload, isBinary):
-        pass
-
-    def connectionLost(self, reason):
-        WebSocketServerProtocol.connectionLost(self, reason)
-        self.factory.unregister(self)
-
-
-class BroadcastServerFactory(WebSocketServerFactory):
-    def __init__(self, url):
-        WebSocketServerFactory.__init__(self, url)
-        self.clients = []
-
-    def register(self, client):
-        if client not in self.clients:
-            logger.info("registered client {}".format(client.peer))
-            self.clients.append(client)
-
-    def unregister(self, client):
-        if client in self.clients:
-            logger.info("unregistered client {}".format(client.peer))
-            self.clients.remove(client)
-
-    def broadcast(self, msg):
-        #logger.info("broadcasting message '{}'".format(msg))
-        for c in self.clients:
-            c.sendMessage(msg.encode('utf8'))
-            logger.debug("message sent to {}".format(c.peer))
-
-
-# relay this...
+topic = u''
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect('tcp://localhost:9002')
-#socket.connect('tcp://localhost:' + str(config.kmet1_port))
-#socket.connect('tcp://' + config.kmet1_ip + ':' + str(config.kmet1_port))
-socket.setsockopt_string(zmq.SUBSCRIBE,u'')
+socket.connect('tcp://localhost:{}'.format(zmq_port))
+socket.setsockopt_string(zmq.SUBSCRIBE,topic)
+poller = zmq.Poller()
+poller.register(socket,zmq.POLLIN)
 
-# ... to this
 ServerFactory = BroadcastServerFactory
-factory = ServerFactory(u'ws://*:9000')
+factory = ServerFactory(u'ws://*:{}'.format(ws_port))
 factory.protocol = BroadcastServerProtocol
 listenWS(factory)
 
 
-poller = zmq.Poller()
-poller.register(socket,zmq.POLLIN)
-def task():
-    socks = dict(poller.poll(1000))
-    if socket in socks and zmq.POLLIN == socks[socket]:
-        s = socket.recv_string()
-        logger.debug(s)
-        factory.broadcast(s)
+def taskListener():
+    try:
+        socks = dict(poller.poll(1000))
+        if socket in socks and zmq.POLLIN == socks[socket]:
+            m = socket.recv_string()
+            print(m)
+            factory.broadcast(m)
+    except:
+        traceback.print_exc()
 
-lc = LoopingCall(task)
-lc.start(0.1)
 
+LoopingCall(taskListener).start(0.01,now=False)
 reactor.run()
-
 socket.close()
-
-logger.info('terminated')
