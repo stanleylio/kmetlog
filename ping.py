0f6f2bb35f54f4cf6619ea0bdc3cee6eb5fb3b17
Stanley Lio <stanleylio@gmail.com>
2017-02-22 20:22:29 +0000

    .

--- ping.py
+++ ping.py
@@ -9,36 +9,36 @@ from __future__ import division
 import sys,time,json,traceback
 from os.path import expanduser
 sys.path.append(expanduser('~'))
-from kmetlog import dbstuff
 from node.helper import ts2dt
 from node.send2server import post
-from kmetlog.storage import storage
+from node.storage.storage2 import storage
 
 
-store = storage('www-data',open('/var/www/km1app/km1app/mysql_cred').read().strip(),'kmetlog')
+store = storage(user='www-data',passwd=open('/var/www/km1app/km1app/mysql_cred').read().strip(),dbname='kmetlog')
 
 D = {}
 end = time.time()
 begin = end - 3600
+print(begin,end)
 for table in store.get_list_of_tables():
     V = {}
     for column in store.get_list_of_columns(table):
         if column in ['ts','OpticalRain_weather_condition']:
             continue
-        #print table,column
+        #print(table,column)
         try:
-            r = store.read_time_range(table,column,begin,end)
-            N = len(r)
-            r = zip(*r)
-            #V[column] = (max(r[0]),round(sum(r[1])/N,3))
-            V[column] = (max(r[0]),round(sum(r[1])/N,3),N)
+            print(table,column,begin,end)
+            r = store.read_time_range(table,'ts',['ts',column],begin,end)
+            N = len(r['ts'])
+            #if N:
+            V[column] = (max(r['ts']),round(sum(r[column])/N,3),N)
         except:
-            pass
+            traceback.print_exc()
     D[table] = V
 
 try:
     m = json.dumps(D,separators=(',',':'))
-    print m
+    print(m)
     print(post(m,'http://grogdata.soest.hawaii.edu/api/4'))
 except:
     m = traceback.format_exc()
