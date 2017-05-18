ec5b680c8616264fb9f5159d87d9b117d9d066bd
Stanley Lio <stanleylio@gmail.com>
2017-05-18 22:19:04 +0000

    all vars stored in the same table

--- log2mysql.py
+++ log2mysql.py
@@ -1,38 +1,68 @@
 # Stanley H.I. Lio
 # hlio@hawaii.edu
+# University of Hawaii
 # All Rights Reserved. 2017
-import zmq,sys,json,traceback,time,math,MySQLdb
-from os.path import expanduser
+import sys,json,traceback,time,math,pika,MySQLdb,logging
+from os.path import expanduser,basename
 sys.path.append(expanduser('~'))
 from datetime import datetime
 from node.storage.storage2 import storage
 from node.parse_support import parse_message,pretty_print
-from node.zmqloop import zmqloop
 from cred import cred
 
 
+logging.basicConfig(level=logging.DEBUG)
+
+
+exchange = 'uhcm'
+table = 'kmetlog'       # unforseen consequence...
+
+
 def init_storage():
-    #store = storage(user='root',passwd=open(expanduser('~/mysql_cred')).read().strip(),dbname='uhcm')
     return storage(user='root',passwd=cred['mysql'],dbname='kmetlog')
 store = init_storage()
 
 
-def callback(m):
+credentials = pika.PlainCredentials('nuc',cred['rabbitmq'])
+connection = pika.BlockingConnection(pika.ConnectionParameters('166.122.96.12',5672,'/',credentials))
+channel = connection.channel()
+channel.exchange_declare(exchange=exchange,type='topic',durable=True)
+result = channel.queue_declare(queue=basename(__file__),
+                               durable=False,
+                               exclusive=True,
+                               auto_delete=True,
+                               arguments={'x-message-ttl':10*1000})
+channel.basic_qos(prefetch_count=1)
+queue_name = result.method.queue
+channel.queue_bind(exchange=exchange,
+                   queue=queue_name,
+                   routing_key='samples')
+
+
+def callback(ch,method,properties,body):
     global store
     try:
-        tmp = m.split(',',1)  # ignore the "kmet1," part
-        d = json.loads(tmp[1])
-        table = d['tag']
-        d = {k:d[k] for k in store.get_list_of_columns(table)}
+        d = json.loads(body)
+        d['ReceptionTime'] = time.time()
+        d = {k:d[k] for k in store.get_list_of_columns(table) if k in d}
         store.insert(table,d)
         print('= = = = = = = = = = = = = = =')
         pretty_print(d)
-    except MySQLdb.OperationalError,e:
-        if e.args[0] in (MySQLdb.constants.CR.SERVER_GONE_ERROR,MySQLdb.constants.CR.SERVER_LOST):
-            store = init_storage()
+    except MySQLdb.OperationalError:
+        traceback.print_exc()
+        store = init_storage()
     except:
         traceback.print_exc()
-        print(m)
-    
+        print(body)
 
-zmqloop(callback,topic=u'kmet1')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    return
+
+
+logging.info(__file__ + ' is ready')
+channel.basic_consume(callback,queue=queue_name)    # ,no_ack=True
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    logging.info('user interrupted')
+logging.info(__file__ + ' terminated')
