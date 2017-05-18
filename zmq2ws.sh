95cfde0546e80ae88d1f4cb57baef4a312e89978
Stanley Lio <stanleylio@gmail.com>
2017-05-18 09:49:31 +0000

    .

--- zmq2ws.sh
+++ zmq2ws.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# doesn't work in supervisor for some reason. not in use.
+# didn't work in supervisor for some reason. not in use.
 exit 0;
-python zmq2stdout.py 127.0.0.1:9002 | python stdin2ws.py 9000
+bash -c "python zmq2stdout.py 127.0.0.1:9002 | python stdin2ws.py 9000"
